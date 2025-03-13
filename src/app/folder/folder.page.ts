f1a1f877a09a1b44c459492c11cc0c0da0120038
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2025-03-13 11:23:00 +0100

    PoC user

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -3,12 +3,12 @@ import { ActivatedRoute } from '@angular/router';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { AuthService } from '../services/auth.service';
 import { User } from '../dtos/user';
-import { NgIf } from '@angular/common';
+import { NgIf, AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, NgIf],
+  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, NgIf, AsyncPipe],
 })
 export class FolderPage implements OnInit {
   public folder!: string;
@@ -18,15 +18,11 @@ export class FolderPage implements OnInit {
   currentUser$ = this.authService.currentUser$;
   isLoggedIn$ = this.authService.isLoggedIn$;
 
-  user = signal<User | null>(null);
 
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
-    this.authService.fetchUser().subscribe((user) => {
-      this.user.set(user);
-    }); 
   }
 
   logout() {
