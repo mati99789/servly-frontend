285102b205f865ac4b9ca9166129d953ff02bd0a
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2025-03-13 11:16:12 +0100

    PoC user

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,19 +1,35 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { AuthService } from '../services/auth.service';
+import { User } from '../dtos/user';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, NgIf],
 })
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  private authService = inject(AuthService);
+
+  currentUser$ = this.authService.currentUser$;
+  isLoggedIn$ = this.authService.isLoggedIn$;
+
+  user = signal<User | null>(null);
+
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.authService.fetchUser().subscribe((user) => {
+      this.user.set(user);
+    }); 
+  }
+
+  logout() {
+    this.authService.logout().subscribe();
   }
 }
